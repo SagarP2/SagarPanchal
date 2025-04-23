da5edbe8a34f5a1a453ba6562ef24d13b0f6896d
Sagar Panchal <panchalsagar992003@gmail.com>
2025-04-23 11:07:28 +0530

    GitHub link add

--- assets/js/main.js
+++ assets/js/main.js
@@ -288,19 +288,10 @@ document.addEventListener("DOMContentLoaded", function () {
               ? `<li><strong>Title:</strong> ${project.title}</li>`
               : ""
           }
-          ${
-            project.category
-              ? `<li><strong>Category:</strong> ${project.category}</li>`
-              : ""
-          }
+          
           ${
             project.purpose
               ? `<li><strong>Purpose:</strong> ${project.purpose}</li>`
-              : ""
-          }
-          ${
-            project.role
-              ? `<li><strong>Role:</strong> ${project.role}</li>`
               : ""
           }
           ${
@@ -310,7 +301,50 @@ document.addEventListener("DOMContentLoaded", function () {
                 )}</li>`
               : ""
           }
-              
+       
+       
+ 
+    ${
+      project.github
+        ? `
+      <li>
+        <a 
+          href="${project.github}" 
+          target="_blank" 
+         
+          class="github-link"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22"  fill="none" stroke="BLACK" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round">
+            <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+          </svg>
+          <span class="github-label">GitHub</span>
+        </a>
+      </li>
+      `
+        : ""
+    }
+
+    ${
+      project.liveDemo
+        ? `
+      <li>
+        <a 
+          href="${project.liveDemo}" 
+          target="_blank" 
+          
+          class="livedemo"
+        >
+          <span class="livelable">Live Demo</span>
+          <i class="bi bi-arrow-up-right"></i>  
+        </a>
+      </li>
+      `
+        : ""
+    }
+ 
+
+
+
         `;
 
         // Add key features
@@ -414,7 +448,7 @@ document.addEventListener("DOMContentLoaded", function () {
               clickable: true,
             },
             navigation: {
-              nextEl: ".swiper-button-next", 
+              nextEl: ".swiper-button-next",
               prevEl: ".swiper-button-prev",
             },
           });
@@ -585,7 +619,9 @@ document.addEventListener("DOMContentLoaded", () => {
             : sortedData.filter((item) => item.category === filterCategory);
 
         // If on index page, limit the displayed projects to 3
-        const projectsToShow = isIndexPage ? filteredData.slice(0, 3) : filteredData;
+        const projectsToShow = isIndexPage
+          ? filteredData.slice(0, 3)
+          : filteredData;
 
         // Add exit animation to all project items
         const currentItems =
